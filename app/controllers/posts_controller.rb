9094917ebb6ad4c288d2d01fc829a23d3befa5a4
kaori kimura <tkhy_1125@icloud.com>
2022-04-09 14:03:40 +0900

    “初回アップ”

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "つぶやきました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "つぶやきを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"ブログを削除しました！"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
